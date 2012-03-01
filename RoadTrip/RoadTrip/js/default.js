291b81ebeeed8a7a999ab3202c5108c6428a1ddb
John Lam <jlam@iunknown.com>
2012-02-29 18:22:04 -0800

    Refactoring map search code to use correct object serialization semantics

--- RoadTrip/RoadTrip/js/default.js
+++ RoadTrip/RoadTrip/js/default.js
@@ -2,15 +2,16 @@
 // http://go.microsoft.com/fwlink/?LinkId=232509
 
 // TODO: secure these credentials and put them in one place
-var credentials = "ArPzSbmnfYJucWzarJwB7Gk29sfWPy97C6rpz-Rm1xTO_AWa-_hCg-5Oo-mAHkcR";
+var bing_maps_key = "ArPzSbmnfYJucWzarJwB7Gk29sfWPy97C6rpz-Rm1xTO_AWa-_hCg-5Oo-mAHkcR";
+var bing_search_key = "2402CFD2EA26B9328072CCBF88051BE17B5547C8";
 
 // TODO: figure out a good place to serialize all of this information - it should
 // probably be some kind of cloud service that I run with a document database.
 // The reasoning behind this is to allow people to easily share their trips and
 // to find trips that intersect?
 var locations = new WinJS.Binding.List([
-    { location: "Seattle, WA" },
-    { location: "Portland, OR" }
+    { location: "Seattle, WA", bbox: [47.25339889526367, -123.16571807861328, 47.946136474609375, -121.5034408569336] },
+    { location: "Portland, OR", bbox: [45.1732063293457, -123.4850082397461, 45.865943908691406, -121.88526153564453] }
 ]);
 
 (function () {
@@ -18,24 +19,35 @@ var locations = new WinJS.Binding.List([
 
     var app = WinJS.Application;
 
-    // Do an XHR request
-    app.search = function (location) {
+    // Search Bing Maps for the location
+    app.search_map = function (location) {
         $.ajax({
-            url: 'http://dev.virtualearth.net/REST/v1/Locations/' + location + '?output=json&key=' + credentials,
+            url: 'http://dev.virtualearth.net/REST/v1/Locations/' + location + '?output=json&key=' + bing_maps_key,
             dataType: 'json',
             success: function (data, text_status) {
                 // Emit the search results as a bindable list of locations that the user can choose from in the flyout
                 var results = new WinJS.Binding.List();
                 for (var i = 0; i < data.resourceSets[0].resources.length; i++) {
-                    results.push({ location: data.resourceSets[0].resources[i].name });
-                    console.log(data.resourceSets[0].resources[i].bbox);
-                    console.log(data.resourceSets[0].resources[i].name);
+                    results.push({
+                        location: data.resourceSets[0].resources[i].name,
+                        bbox: data.resourceSets[0].resources[i].bbox
+                    });
                 }
                 var resultsList = $("#searchResultsList")[0].winControl;
                 resultsList.itemDataSource = results.dataSource;
                 resultsList.itemTemplate = $("#searchResultsTemplate")[0];
             }
         });
+    };
+
+    // Search Bing for the search term
+    app.zoom_to = function (bbox) {
+        var message = {
+            action: 'zoom_to',
+            bbox: bbox,
+            credentials: bing_maps_key
+        };
+        document.frames["map"].postMessage(message, "ms-appx-web://" + document.location.host);
     };
 
     app.onactivated = function (eventObject) {
@@ -47,22 +59,23 @@ var locations = new WinJS.Binding.List([
                 // Bind event handlers
                 $("#list")[0].winControl.addEventListener("iteminvoked", function (eventObject) {
                     eventObject.detail.itemPromise.then(function (invokedItem) {
-                        document.frames["map"].postMessage(invokedItem.data.location, "ms-appx-web://" + document.location.host);
-                        console.log("clicked on: " + invokedItem.data.location);
+                        app.zoom_to(invokedItem.data.bbox);
                     });
                 }, false);
 
                 $("#buttonSearch").click(function () {
-                    app.search($("#searchInput")[0].value);
+                    app.search_map($("#searchInput")[0].value);
                 });
 
                 // TODO: note that this is probably not the right heuristic to use here - when the user searches for
                 // something they might want to see where it is beyond what the location text is ... need to test this further
                 $("#searchResultsList")[0].winControl.addEventListener("iteminvoked", function (eventObject) {
                     eventObject.detail.itemPromise.then(function (invokedItem) {
-                        // Add to the locations list the new item and jump there
-                        document.frames["map"].postMessage(invokedItem.data.location, "ms-appx-web://" + document.location.host);
-                        locations.push({ location: invokedItem.data.location });
+                        app.zoom_to(invokedItem.data.bbox);
+                        locations.push({
+                            location: invokedItem.data.location,
+                            bbox: invokedItem.data.bbox
+                        });
 
                         // Dismiss the flyout control
                         $("#searchFlyout")[0].winControl.hide();
--- RoadTrip/RoadTrip/js/map.js
+++ RoadTrip/RoadTrip/js/map.js
@@ -1,38 +1,37 @@
 ﻿(function () {
-    // TODO: secure these credentials
-    var credentials = "ArPzSbmnfYJucWzarJwB7Gk29sfWPy97C6rpz-Rm1xTO_AWa-_hCg-5Oo-mAHkcR";
     var map = null;
 
-    $(document).ready(function () {
-        var mapOptions = {
-            credentials: credentials,
-            center: new Microsoft.Maps.Location(47.733762, -122.146974),
-            mapTypeId: Microsoft.Maps.MapTypeId.road,
-            zoom: 7,
-            showLogo: false
-        };
-        map = new Microsoft.Maps.Map($("#map")[0], mapOptions);
-        search("miami, fl");
-    });
+    get_map = function (message) {
+        if (map == null) {
+            var mapOptions = {
+                credentials: message.credentials,
+                center: new Microsoft.Maps.Location(47.733762, -122.146974),
+                mapTypeId: Microsoft.Maps.MapTypeId.road,
+                zoom: 7,
+                showLogo: false
+            };
+            map = new Microsoft.Maps.Map($("#map")[0], mapOptions);
+        }
+        return map;
+    }
 
-    search = function (query) {
-        var searchRequest = 'http://dev.virtualearth.net/REST/v1/Locations/' + query + '?output=json&jsonp=search_callback&key=' + credentials;
-        var mapscript = document.createElement('script');
-        mapscript.type = 'text/javascript';
-        mapscript.src = searchRequest;
-        document.getElementById('map').appendChild(mapscript);
-    };
-
-    search_callback = function (result) {
-        var bbox = result.resourceSets[0].resources[0].bbox;
-        var topLeft = new Microsoft.Maps.Location(bbox[0], bbox[1]);
-        var bottomRight = new Microsoft.Maps.Location(bbox[2], bbox[3]);
+    zoom_to = function (message) {
+        var topLeft = new Microsoft.Maps.Location(message.bbox[0], message.bbox[1]);
+        var bottomRight = new Microsoft.Maps.Location(message.bbox[2], message.bbox[3]);
         var rect = new Microsoft.Maps.LocationRect.fromLocations(topLeft, bottomRight);
-        map.setView({ bounds: rect });
+        get_map(message).setView({ bounds: rect });
     };
 
     receive_message = function (e) {
-        search(e.data);
+        var message = e.data;
+        switch (e.data.action) {
+            case 'zoom_to':
+                zoom_to(message);
+                break;
+            default:
+                console.log('invalid action: ' + message.action);
+                break;
+        }
     };
     
     window.addEventListener("message", receive_message, false);
