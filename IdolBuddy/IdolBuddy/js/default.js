5f9255688e7d5d6172d605c671352a2b03e68054
John Lam <jlam@iunknown.com>
2012-03-08 17:14:25 -0800

    More progress on Idol Buddy

--- IdolBuddy/IdolBuddy/js/default.js
+++ IdolBuddy/IdolBuddy/js/default.js
@@ -7,16 +7,55 @@ var idols = new WinJS.Binding.List([
     { name: 'Deandre Brackensick', photo: 'http://media.americanidol.com/contestants/season_11/5a9dkg8g6e/top_24/deandre_brackensick.jpg', twitter: 'BracensickAI11', facebook: 'BrackensickAI11' },
     { name: 'Elise Testone', photo: 'http://media.americanidol.com/contestants/season_11/5a9dkg8g6e/top_24/elise_testone.jpg', twitter: 'ETestoneAI11', facebook: 'ETestoneAI11' },
     { name: 'Erika Van Pelt', photo: 'http://media.americanidol.com/contestants/season_11/5a9dkg8g6e/top_24/erika_van_pelt.jpg', twitter: 'EVanPeltAI11', facebook: 'EVanPeltAI11' },
-    { name: 'Heejun Han', photo: 'http://media.americanidol.com/contestants/season_11/5a9dkg8g6e/top_24/erika_van_pelt.jpg', twitter: 'HHanAI11', facebook: 'HHanAI11' },
+    { name: 'Heejun Han', photo: 'http://media.americanidol.com/contestants/season_11/5a9dkg8g6e/top_24/heejun_han.jpg', twitter: 'HHanAI11', facebook: 'HHanAI11' },
     { name: 'Hollie Cavanagh', photo: 'http://media.americanidol.com/contestants/season_11/5a9dkg8g6e/top_24/hollie_cavanagh.jpg', twitter: 'CavanaghAI11', facebook: 'CavanaghAI11' },
     { name: 'Jeremy Rosado', photo: 'http://media.americanidol.com/contestants/season_11/5a9dkg8g6e/top_24/jeremy_rosado.jpg', twitter: 'JRosadoAI11', facebook: 'JRosadoAI11' },
-    { name: 'Jermaine Jones', photo: 'http://media.americanidol.com/contestants/season_11/5a9dkg8g6e/top_24/jeremy_rosado.jpg', ttwitter: 'JJonesAI11', facebook: 'JJonesAI11' },
+    { name: 'Jermaine Jones', photo: 'http://media.americanidol.com/contestants/season_11/5a9dkg8g6e/top_24/jermaine_jones.jpg', ttwitter: 'JJonesAI11', facebook: 'JJonesAI11' },
     { name: 'Jessica Sanchez', photo: 'http://media.americanidol.com/contestants/season_11/5a9dkg8g6e/top_24/jessica_sanchez.jpg', ttwitter: 'JSanchezAI11', facebook: 'JSanchezAI11' },
     { name: 'Joshua Ledet', photo: 'http://media.americanidol.com/contestants/season_11/5a9dkg8g6e/top_24/joshua_ledet.jpg', ttwitter: 'JLedetAI11', facebook: 'JLedetAI11' },
     { name: 'Philip Phillips', photo: 'http://media.americanidol.com/contestants/season_11/5a9dkg8g6e/top_24/phillip_phillips.jpg', ttwitter: 'PPhillipsAI11', facebook: 'PPhillipsAI11' },
     { name: 'Shannon Magrane', photo: 'http://media.americanidol.com/contestants/season_11/5a9dkg8g6e/top_24/shannon_magrane.jpg', ttwitter: 'SMagraneAI11', facebook: 'SMagraneAI11' },
     { name: 'Skylar Laine', photo: 'http://media.americanidol.com/contestants/season_11/5a9dkg8g6e/top_24/skylar_laine.jpg', ttwitter: 'SLaineAI11', facebook: 'SLaineAI11' }
 ]);
+
+function item_template(item_promise) {
+    return item_promise.then(function (current_item) {
+        var result = document.createElement('div');
+        result.className = 'idol-item';
+
+        var image = document.createElement('img');
+        image.className = 'idol-photo';
+        image.src = current_item.data.photo;
+        result.appendChild(image);
+
+        var overlay = document.createElement('div');
+        overlay.className = 'idol-overlay';
+
+        var caption = document.createElement('h4');
+        caption.className = 'idol-title';
+        caption.textContent = current_item.data.name;
+        overlay.appendChild(caption);
+
+        var subtitle = document.createElement('h6');
+        subtitle.className = 'idol-subtitle';
+        subtitle.textContent = '10,485';
+        overlay.appendChild(subtitle);
+
+        result.appendChild(overlay);
+        return result;
+    });
+};
+
+var follower_count_query = 'https://api.twitter.com/1/users/show.json?screen_name=';
+
+// TODO: Cache in offline store
+function get_twitter_follower_count(name) {
+    WinJS.xhr({
+    }).then(function (complete) {
+    }, function (error) {
+    }, function (progress) {
+    });
+};
 
 (function () {
     "use strict";
