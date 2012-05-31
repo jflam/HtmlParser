6dcef040129d102533da975aa550d8ea3e8732af
John Lam <jlam@iunknown.com>
2012-05-30 18:15:18 -0700

    Added search contract integration

--- TechEd/step3/Editor/js/default.js
+++ TechEd/step3/Editor/js/default.js
@@ -49,6 +49,57 @@
             },
             fallthrough: ["default"],
         };
+
+        var xhrRequest = null;
+
+        // Provide suggestions using an URL that supports the Open Search suggestion format.
+        // Scenarios 2-6 introduce different methods of providing suggestions. The registration for the onsuggestionsrequested
+        // event is added in a local scope for this sample's purpose, but in the common case, you should place this code in the
+        // global scope, e.g. default.js, to run as soon as your app is launched. This way your app can provide suggestions
+        // anytime the user brings up the search pane.
+        Windows.ApplicationModel.Search.SearchPane.getForCurrentView().onsuggestionsrequested = function (eventObject) {
+            var queryText = eventObject.queryText, language = eventObject.language, suggestionRequest = eventObject.request;
+
+            // The deferral object is used to supply suggestions asynchronously for example when fetching suggestions from a web service.
+            // Indicate that we'll obtain suggestions asynchronously:
+            var deferral = suggestionRequest.getDeferral();
+
+            // This refers to a local package file that contains a sample JSON response.
+            // You can update the Uri to a service that supports this standard in order to see suggestions come from a web service.
+            var suggestionUri = "http://en.wikipedia.org/w/api.php?action=opensearch&search=";
+            // If you are using a webservice,the query string should be encoded into the URI. See example below:
+            suggestionUri += encodeURIComponent(queryText);
+
+            // Cancel the previous suggestion request if it is not finished.
+            if (xhrRequest && xhrRequest.cancel) {
+                xhrRequest.cancel();
+            }
+
+            // Create request to obtain suggestions from service and supply them to the Search Pane.
+            xhrRequest = WinJS.xhr({ url: suggestionUri });
+            xhrRequest.done(
+                function (request) {
+                    if (request.responseText) {
+                        var parsedResponse = JSON.parse(request.responseText);
+                        if (parsedResponse && parsedResponse instanceof Array) {
+                            var suggestions = parsedResponse[1];
+                            if (suggestions) {
+                                suggestionRequest.searchSuggestionCollection.appendQuerySuggestions(suggestions);
+                                WinJS.log && WinJS.log("Suggestions provided for query: " + queryText, "sample", "status");
+                            } else {
+                                WinJS.log && WinJS.log("No suggestions provided for query: " + queryText, "sample", "status");
+                            }
+                        }
+                    }
+
+                    deferral.complete(); // Indicate we're done supplying suggestions.
+                },
+                function (error) {
+                    WinJS.log && WinJS.log("Error retrieving suggestions for query: " + queryText, "sample", "status");
+                    // Call complete on the deferral when there is an error.
+                    deferral.complete();
+                });
+        };
     };
 
     app.open_file = function (args) {
